6d9407dc4de1ea5bef8ba822a502f80223bc5065
jescobedo <jescobedo@rmn.com>
2016-05-02 12:25:16 -0500

    Major changes

--- js/hangman.js
+++ js/hangman.js
@@ -2,9 +2,11 @@ var input = $('#input'); // text area for input
 var man = $('#man'); // Hangman drawing
 var wordDisplay = $('#wordDisplay'); // word guessed so far display
 var gameState = $('#gameState'); // win or lose section
+var keyboard = $('#keyboard'); // win or lose section
 
 var guesses = 0; // Number of wrong guesses made
-var wordlist = ["wisper", "yodel", "whisky", "popcorn", "shogun", "easter"];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'x', 'z'];
+var wordlist = ["wisper", "yodel", "whisky", "popcorn", "shogun", "easter","taxonomy","bluebird","dynamic"];
 
 var hangmanArt = // each line counts as a guess
   [
@@ -15,20 +17,38 @@ var hangmanArt = // each line counts as a guess
     ["/ \\\n"]
   ];
 
-function createWord(phrase) { // Takes a word and turns in into a word object to be guessed
+
+// Converts word into word object to be guessed
+function createWord(phrase) { 
   word = [];
   for (var i = 0; i < phrase.length; i++) {
     word.push([phrase[i], false])
   }
 }
 
-function getGuess() { // Takes letter from text box and calls "guess()" with it
-  var letter = input.val();
-  guess(letter);
+function createKeyboard(alphabet) {
+  
+  for (var i = 0; i < alphabet.length; i++) {
+    var letter = alphabet[i];
+    var key = $("<div></div>").addClass('key');
+    key.html(letter);
+    keyboard.append(key);
+    if ( i%9 === 8 ){
+      keyboard.append("<br>");
+    }
+  }
+  
+  keyboard.click(function (e) {
+      var $target = $(e.target);
+      var targetLetter = $target.text(); //e.target.innerHTML;
+      guess(targetLetter);
+    })
 }
 
-function guess(letter) { // Takes letter and checks to see if word object contains it
+// Takes letter and checks to see if word object contains it
+function guess(letter) { 
   letterFound = false;
+  console.log(letter);
   for (var i = 0; i < word.length; i++) { // iterate through word for matches, change flag to true
     if (word[i][0] === letter) {
       letterFound = true;
@@ -85,8 +105,7 @@ function makeWordString() { // iterate word object, show letter if true, dash if
 }
 
 // Main
+createKeyboard(alphabet);
 createWord(wordlist[Math.floor(Math.random() * wordlist.length)]);
 drawMan();
 drawWord();
-
-$('#guess').on('click', getGuess);
